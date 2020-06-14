d32383d3b0d3b9ca34c87ad59b6d8ea0b20a85a6
Aries Cs <aries0d0f@gmail.com>
2020-06-14 14:26:43 +0800

    [Deploy] Add content loader & parser.

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,14 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import metadataParser from "markdown-yaml-metadata-parser";
+
+import rawParagraph from "!!raw-loader!@/../post/index.md";
 
 @Component
-export default class App extends Vue {}
+export default class App extends Vue {
+  private metadata = metadataParser(rawParagraph).metadata;
+}
 </script>
 
 <style lang="scss">
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,41 @@
 <template>
   <div class="home">
-    <!-- PLACEHOLDER -->
+    <article class="article-container" v-html="paragraphs" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import marked from "marked";
+import metadataParser from "markdown-yaml-metadata-parser";
 
-@Component
-export default class Home extends Vue {}
+import rawParagraph from "!!raw-loader!@/../post/index.md";
+
+import Heading from "@/components/element/Heading.vue";
+
+@Component({
+  components: {
+    Heading
+  }
+})
+export default class Home extends Vue {
+  private paragraphs = marked(
+    metadataParser(rawParagraph)
+      .content.replace(
+        /<!-- article::start -->/g,
+        `<div class="article article-part">`
+      )
+      .replace(/<!-- article::end -->/g, `</div>`)
+      .replace(
+        /<!-- spotlight::full-start -->/g,
+        `<div class="spotlight spotlight-fullsize">`
+      )
+      .replace(/<!-- spotlight::full-end -->/g, `</div>`)
+      .replace(
+        /<!-- spotlight::reader-start -->/g,
+        `<div class="spotlight spotlight-reader">`
+      )
+      .replace(/<!-- spotlight::reader-end -->/g, `</div>`)
+  );
+}
 </script>
