ca915d7e93f966f6b69f1e081163daab3a585bb2
Aries Cs <aries0d0f@gmail.com>
2020-06-19 00:53:30 +0800

    [Layout] Adjustable progressbar.

--- src/components/layout/Banner.vue
+++ src/components/layout/Banner.vue
@@ -3,7 +3,10 @@
     <div
       v-for="(banner, index) in bannerList"
       :key="banner.img"
-      :class="{ active: index === activeBannerIndex % 7 }"
+      :class="[
+        { active: index === activeBannerIndex % 8 },
+        banner.progressStyle
+      ]"
       :style="{
         'background-image': `url(${require('@/assets/img/banner/' +
           banner.img)})`,
@@ -20,44 +23,53 @@ import { Component, Vue } from "vue-property-decorator";
 
 @Component
 export default class Banner extends Vue {
-  private activeBannerIndex = 0;
+  private activeBannerIndex = -1;
 
   private bannerList = [
     {
       img: "banner0.png",
-      gravity: "top right"
+      gravity: "top right",
+      progressStyle: "dark"
     },
     {
       img: "banner1.png",
-      gravity: "bottom left"
+      gravity: "bottom left",
+      progressStyle: "light"
     },
     {
       img: "banner2.png",
-      gravity: "center center"
+      gravity: "center center",
+      progressStyle: "dark"
     },
     {
       img: "banner3.png",
-      gravity: "bottom right"
+      gravity: "bottom right",
+      progressStyle: "light"
     },
     {
       img: "banner4.png",
-      gravity: "center center"
+      gravity: "center center",
+      progressStyle: "light"
     },
     {
       img: "banner5.png",
-      gravity: "top left"
+      gravity: "top left",
+      progressStyle: "dark"
     },
     {
       img: "banner6.png",
-      gravity: "bottom center"
+      gravity: "bottom center",
+      progressStyle: "dark"
     },
     {
       img: "banner7.png",
-      gravity: "bottom center"
+      gravity: "bottom center",
+      progressStyle: "light"
     }
   ];
 
   private mounted(): void {
+    setTimeout(this.timmerHandler, 100);
     setInterval(this.timmerHandler, 10000);
   }
 
