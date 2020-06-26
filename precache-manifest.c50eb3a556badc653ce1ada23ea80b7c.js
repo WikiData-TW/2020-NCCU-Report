ad53dd956dc4982ddb5d61ddb0b714488fb2f633
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-26 08:05:10 +0000

    Deploy WikiData-TW/2020-NCCU-Report to github.com/WikiData-TW/2020-NCCU-Report.git:gh-pages

---
+++ precache-manifest.c50eb3a556badc653ce1ada23ea80b7c.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29e0959106e6adf6a82bb08e7e8c4451",
+    "url": "/cooperation/NCCU-news-2020/app.html"
+  },
+  {
+    "revision": "ebca34a52215de44da65",
+    "url": "/cooperation/NCCU-news-2020/css/app.a6637e4f.css"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/cooperation/NCCU-news-2020/img/.gitkeep"
+  },
+  {
+    "revision": "235af55c653311ee9ed7b4550614d959",
+    "url": "/cooperation/NCCU-news-2020/img/BG1.235af55c.jpg"
+  },
+  {
+    "revision": "830329840698844c2370528c596c07b4",
+    "url": "/cooperation/NCCU-news-2020/img/BG2.83032984.jpg"
+  },
+  {
+    "revision": "a9f675c0412c9fe86418cf7bdc3655ff",
+    "url": "/cooperation/NCCU-news-2020/img/BG3.a9f675c0.jpg"
+  },
+  {
+    "revision": "82ce5c2e1f5e5054a347b8d533687189",
+    "url": "/cooperation/NCCU-news-2020/img/BG4.82ce5c2e.jpg"
+  },
+  {
+    "revision": "f8a480bb46ee4e4c4d0d7f70e96aede9",
+    "url": "/cooperation/NCCU-news-2020/img/BG5.f8a480bb.jpg"
+  },
+  {
+    "revision": "c9bbeb6840fd929a1797da466e179d38",
+    "url": "/cooperation/NCCU-news-2020/img/BG6.c9bbeb68.jpg"
+  },
+  {
+    "revision": "292488426b13774b42f4b520e1f33c11",
+    "url": "/cooperation/NCCU-news-2020/img/UniversityNewsLogo.29248842.png"
+  },
+  {
+    "revision": "dd2856667b27bbcd1bfb4ffb26d4fe20",
+    "url": "/cooperation/NCCU-news-2020/img/applicate-flow.dd285666.png"
+  },
+  {
+    "revision": "551c84b3ce551ba5bcf22dc6da5d8ca8",
+    "url": "/cooperation/NCCU-news-2020/img/banner0.551c84b3.png"
+  },
+  {
+    "revision": "a38849d2d5992f19369aff47d87b1f55",
+    "url": "/cooperation/NCCU-news-2020/img/banner1.a38849d2.png"
+  },
+  {
+    "revision": "e0a7dc77642babd0793cb3363a3648f2",
+    "url": "/cooperation/NCCU-news-2020/img/banner2.e0a7dc77.png"
+  },
+  {
+    "revision": "1208ad2f0a319bdb78d8c1022d27311b",
+    "url": "/cooperation/NCCU-news-2020/img/banner3.1208ad2f.png"
+  },
+  {
+    "revision": "e3c2e868e46ee690b64ea2ac0709dc40",
+    "url": "/cooperation/NCCU-news-2020/img/banner4.e3c2e868.png"
+  },
+  {
+    "revision": "92bec9a114c8131ce9c51edcb06b7c16",
+    "url": "/cooperation/NCCU-news-2020/img/banner5.92bec9a1.png"
+  },
+  {
+    "revision": "1cc1d68e91256d6a948f11a34b9547dc",
+    "url": "/cooperation/NCCU-news-2020/img/banner6.1cc1d68e.png"
+  },
+  {
+    "revision": "ef5300c0f0dd57516cbad3edbe303272",
+    "url": "/cooperation/NCCU-news-2020/img/banner7.ef5300c0.png"
+  },
+  {
+    "revision": "c170c01e2583c98ef7ab03a1cafe458a",
+    "url": "/cooperation/NCCU-news-2020/img/bird.c170c01e.jpg"
+  },
+  {
+    "revision": "e001c610403f7194373593e7f4944731",
+    "url": "/cooperation/NCCU-news-2020/img/clamsline1.e001c610.png"
+  },
+  {
+    "revision": "4beb3ecb71083ae20ae0a3dfeb11c9c3",
+    "url": "/cooperation/NCCU-news-2020/img/clamsline2.4beb3ecb.png"
+  },
+  {
+    "revision": "5eedf8fdde023d593d0041dbd053d90e",
+    "url": "/cooperation/NCCU-news-2020/img/clamsline3.5eedf8fd.png"
+  },
+  {
+    "revision": "5b252a0c713836ed23f45fb8beb7b2db",
+    "url": "/cooperation/NCCU-news-2020/img/clamsline4.5b252a0c.png"
+  },
+  {
+    "revision": "0dd8e7512f1cec80772c1553c1f7a237",
+    "url": "/cooperation/NCCU-news-2020/img/clamsline5.0dd8e751.png"
+  },
+  {
+    "revision": "8d7c08cf32eeea55412e573143723f33",
+    "url": "/cooperation/NCCU-news-2020/img/consult-flow.8d7c08cf.png"
+  },
+  {
+    "revision": "0cdbfdd2c5bd74ccb9b09029f9513c0f",
+    "url": "/cooperation/NCCU-news-2020/img/fisherman1.0cdbfdd2.jpg"
+  },
+  {
+    "revision": "79c39306f94d47f233b6b8b9f2ccbfd6",
+    "url": "/cooperation/NCCU-news-2020/img/fisherman2.79c39306.jpg"
+  },
+  {
+    "revision": "48bb893c5a6a652615a3de9eef8be273",
+    "url": "/cooperation/NCCU-news-2020/img/fisherman3.48bb893c.jpg"
+  },
+  {
+    "revision": "49465dd4665a82f809653d77598fcf68",
+    "url": "/cooperation/NCCU-news-2020/img/future-applicate-flow.49465dd4.png"
+  },
+  {
+    "revision": "92502cdd6174e35f921d1a881948f7c2",
+    "url": "/cooperation/NCCU-news-2020/img/house.92502cdd.jpg"
+  },
+  {
+    "revision": "23167bfcdb22af852982499182a48018",
+    "url": "/cooperation/NCCU-news-2020/img/og.jpg"
+  },
+  {
+    "revision": "e56928516a536d13372be55865a971ee",
+    "url": "/cooperation/NCCU-news-2020/img/park-example.e5692851.svg"
+  },
+  {
+    "revision": "74fbfd624be5c156f60e4975f74e3891",
+    "url": "/cooperation/NCCU-news-2020/img/solar-10-years.74fbfd62.png"
+  },
+  {
+    "revision": "f0d99515073ea34324713303c3a3d09a",
+    "url": "/cooperation/NCCU-news-2020/img/solar-panel-target.f0d99515.png"
+  },
+  {
+    "revision": "ebca34a52215de44da65",
+    "url": "/cooperation/NCCU-news-2020/js/app.ba80104d.js"
+  },
+  {
+    "revision": "2cced2572645e6a0116e",
+    "url": "/cooperation/NCCU-news-2020/js/chunk-vendors.b6a7a145.js"
+  },
+  {
+    "revision": "ce8c7328ae764c2637e7e9b1ebfb9787",
+    "url": "/cooperation/NCCU-news-2020/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/cooperation/NCCU-news-2020/robots.txt"
+  }
+]);
